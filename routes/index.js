8f6ee5dc0516a958de161b1d52d8715bdc8485cd
panforrest <guoqianp@gmail.com>
2018-02-27 23:46:30 -0500

    29:58 create a new route to render dynamic data

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
+const superagent = require('superagent')
 
 /*  This is the home route. It renders the index.mustache page from the views directory.
 	Data is rendered using the Mustache templating engine. For more
@@ -14,6 +15,15 @@ router.get('/', (req, res) => {
     }
 
 	res.render('index', data)
+})
+
+router.get('/:username', (req, res) => {
+	const username = req.params.username
+
+	res.json({
+		confirmation: 'success',
+		username: username
+    })
 })
 
 module.exports = router
